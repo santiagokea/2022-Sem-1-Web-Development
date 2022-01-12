2137546ea1c56f778df25eeba532cd1e0cb9f841
Santiago Donoso <sand@kea.dk>
2022-01-12 10:09:18 +0100

    new test

---
+++ app.py
@@ -0,0 +1,16 @@
+from bottle import run, get
+
+
+
+##############################
+@get("/")
+def do():
+  return "x"
+
+
+
+
+
+
+##############################
+run(host="127.0.0.1", port=55555, debug=True, reloader=True)
