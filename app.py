bd0838b5d83abb21679105c2925be654f62eba0e
Santiago Donoso <sand@kea.dk>
2022-02-08 10:49:36 +0100

    ...

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 from bottle import error, get, run, static_file, view
 
+items = [
+  {"id":"b0bafe8f-de0e-4fb2-b3cb-f284e9d5e2ce", "name":"a", "price": 10},
+  {"id":"d097c638-83d5-4ec5-a1db-51e1a09767a7", "name":"b", "price": 20},
+  {"id":"43119a37-98a0-4e77-8ff4-b724cbaf0d3d", "name":"c", "price": 30}
+]
+
+
 ##############################
 @get("/app.css")
 def _():
@@ -15,7 +22,7 @@ def _():
 @get("/items")
 @view("items")
 def _():
-  return
+  return dict(items=items)
 
 ##############################
 @get("/signup")
