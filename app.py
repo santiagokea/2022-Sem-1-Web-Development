d347e23138f311c2a4b939b619c3caa3b62db243
Santiago Donoso <sand@kea.dk>
2022-02-08 09:31:04 +0100

    ...

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from bottle import get, run
+from bottle import error, get, run, view
 
 ##############################
 @get("/")
+@view("index")
 def _():
-  return "home"
+  return
 
 ##############################
+@error(404)
+@view("404")
+def _(error):
+  print(error)
+  return
 
 ##############################
 run(host="127.0.0.1", port=3333, debug=True, reloader=True)
