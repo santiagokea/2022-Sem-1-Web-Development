46160852e99400a449c2e28b704ee6ef6c884af5
Santiago Donoso <sand@kea.dk>
2022-02-11 08:51:43 +0100

    ...

--- app.py
+++ app.py
@@ -1,44 +1,15 @@
-from bottle import error, get, post, redirect, request, run, static_file, view
+from bottle import get, run, view
 
 ##############################
-import home_get             # GET  
-import signup_get           # GET   
-import login_get            # GET
-import users_get            # GET
-import items_get            # GET
-import admin_get            # GET
-import signup_ok_get        # GET
-
-import signup_post          # POST
-import login_post           # POST
-import delete_item_post     # POST
-
-##############################
-@get("/app.css")
+@get("/login")
+@view("login")
 def _():
-  return static_file("app.css", root=".")
+  return 
 
 ##############################
-@error(404)
-@view("404")
-def _(error):
-  print(error)
-  return
+
+
 
 ##############################
-run(host="127.0.0.1", port=3333, debug=True, reloader=True, server="paste")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+run(host="127.0.0.1", port=3333, debug=True, reloader=True)
 
---
+++ thu-10-feb/app.py
@@ -0,0 +1,45 @@
+from bottle import error, get, post, redirect, request, run, static_file, view
+
+
+##############################
+import home_get             # GET  
+import signup_get           # GET   
+import login_get            # GET
+import users_get            # GET
+import items_get            # GET
+import admin_get            # GET
+import signup_ok_get        # GET
+
+import signup_post          # POST
+import login_post           # POST
+import delete_item_post     # POST
+
+##############################
+@get("/app.css")
+def _():
+  return static_file("app.css", root=".")
+
+##############################
+@error(404)
+@view("404")
+def _(error):
+  print(error)
+  return
+
+##############################
+run(host="127.0.0.1", port=3333, debug=True, reloader=True, server="paste")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
