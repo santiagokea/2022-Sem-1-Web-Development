eb7c0c2821a3d1290f09a58b46910b242fa5ca7a
Santiago Donoso <sand@kea.dk>
2022-02-24 11:53:55 +0100

    ...

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
-from bottle import default_app, get, run
+from bottle import default_app, get, run, view
 
 ##############################
 @get("/")
+@view("index")
 def _():
   return
 
 ##############################
 @get("/about-us")
+@view("about-us")
 def _():
   return 
 
 ##############################
 @get("/contact-us")
+@view("contact-us")
 def _():
   return
 
