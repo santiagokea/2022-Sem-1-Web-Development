030584866a7f6fc6c2c7ee1f225feafae444c0e9
Santiago Donoso <sand@kea.dk>
2022-03-10 10:09:01 +0100

    ...

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def _():
 
 
 ##############################
-@post("/tweet")
+@post("/tweets")
 def _():
   # Validate
   # Connect to the db
@@ -24,8 +24,15 @@ def _():
   }
   return tweet
 
-
-
+##############################
+@delete("/tweets/<tweet_id>")
+def _(tweet_id):
+  # Validate
+  # Connect to the db
+  # Delete the tweet
+  # If tweet deleted, send a 200
+  # If tweet not found, send a 400 - 204
+  return tweet_id
 
 
 
